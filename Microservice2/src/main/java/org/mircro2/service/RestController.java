9aa3359ae45a3415f0a4190e80e89b2877d434fc
Abhinav <abhisetu610@gmail.com>
2024-12-29 21:22:00 +0530

    Locabalanced in RestTemplate

--- Microservice2/src/main/java/org/mircro2/service/RestController.java
+++ Microservice2/src/main/java/org/mircro2/service/RestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalTime;
 import java.util.List;
@@ -12,6 +13,10 @@ import java.util.List;
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/web")
 public class RestController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
     @Autowired
     FeignImpl feign;
 
@@ -19,6 +24,13 @@ public class RestController {
     public List<Employee> getData(@PathVariable("id") Integer id, @PathVariable("name") String name)
     {
        return feign.allUser(id,name);
+    }
+
+    @GetMapping("/allEmployee")
+    List<Employee> allUser()
+
+    {
+        return restTemplate.getForObject("http://MICROSERVICE1/api/allEmployee",List.class);
     }
 
     @GetMapping("/getCurrentTime")
