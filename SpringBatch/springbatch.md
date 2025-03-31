44f3cad9dd6f54310af87e97afd85e490dfa7512
Abhinav <abhisetu610@gmail.com>
2025-03-31 11:52:48 +0530

    Add documents

---
+++ SpringBatch/springbatch.md
@@ -0,0 +1,5 @@
+Content:
+
+1. Job / Step and Reader listener
+2. Tasklet
+3. Two types of readers , manual and JdbcCursorItemReader
--- SpringBatch/src/main/java/TaskeletClass.java
+++ SpringBatch/src/main/java/TaskeletClass.java
@@ -3,6 +3,10 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
+/**
+ * A Tasklet is a simple interface in Spring Batch used for single-task steps (e.g., file cleanup, sending notifications, database updates).
+ * Unlike chunk-based processing, a Tasklet runs once per step execution.
+ */
 public class TaskeletClass implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
