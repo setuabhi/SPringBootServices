28dd102103b8eef24c437d1a63e4bbc68c4902d4
Abhinav <abhisetu610@gmail.com>
2025-06-21 13:45:58 +0530

    SAGA

--- InterviewPrep/src/main/java/ChoreographyBasedSaga.java
+++ InterviewPrep/src/main/java/ChoreographyBasedSaga.java
@@ -2,38 +2,49 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public class ChoreographyBasedSaga {
-
+/*
     // Microservice 1
-  /*  @PostMapping("/PlaceOrder")
+    @PostMapping("/PlaceOrder")
     public void createOrder(@RequestBody Order order) {
-        orderRepository.save(order);
-        kafkaTemplate.send("order_created", order.getId()); //sends order id to order_created
+        Boolean status = orderRepository.save(order);
+        if (status) {
+            kafkaTemplate.send("order_success", order); //sends order id to order_created
+        } else {
+            orderRepository.cancel(order);
+        }
+
+
+        @KafkaListener(topics = "order_failed")
+        {
+            orderRepository.cancel(order);
+        }
     }
 
     // Microservice 2
-    @KafkaListener(topics = "order_created")
-    public void processPayment(String orderId) { //reads order id from order_created and checks it's status after payment
-        boolean success = paymentGateway.charge(orderId);
+    @KafkaListener(topics = "order_success")
+    public void processPayment(Order order) { //reads order id from order_created and checks it's status after payment
+        boolean success = paymentGateway.charge(order);
         if (success) {
             kafkaTemplate.send("payment_success", orderId); //sends order id to payment_success
         } else {
-            kafkaTemplate.send("payment_failed", orderId);  //sends order id to payment_failed
+            paymentGateway.rollback(order);
+            kafkaTemplate.send("order_failed", orderId);  //sends order id to order_failed to rollback the  txn / DB updates done in createOrder to maintain Atomicity
+        }
+        @KafkaListener(topics = "payment_failed")
+        {
+            paymentGateway.rollback(order);
         }
     }
 
     // Microservice 3
     @KafkaListener(topics = "payment_success")
     public void updateInventory(String orderId) { //reads orderId from payment_success and update it's inventory
-        inventoryRepository.updateStock(orderId);
-        kafkaTemplate.send("inventory_updated", orderId); //sends order id to inventory_updated
+        boolean success = inventoryRepository.updateStock(orderId);
+        if (false) {
+            kafkaTemplate.send("order_failed", orderId); //sends order id to order_failed to rollback the  txn / DB updates done in createOrder to maintain Atomicity
+            kafkaTemplate.send("payment_failed", orderId); //sends order id to payment_failed to rollback the  txn / DB updates done in processPayment to maintain Atomicity
+        }
     }
 
-    // Microservice 4
-    @KafkaListener(topics = "payment_failed")
-    public void cancelOrder(String orderId) { //reads orderId from payment_failed and cancel order
-        orderRepository.cancelOrder(orderId);
-        kafkaTemplate.send("order_cancelled", orderId); //sends order id to order_cancelled
-    }
 */
-
 }
--- InterviewPrep/src/main/java/OrchestrationBasedSaga.java
+++ InterviewPrep/src/main/java/OrchestrationBasedSaga.java
@@ -4,10 +4,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class OrchestrationBasedSaga {
 
     // centralOrchestrator microservice
-  /*  @PostMapping("/PlaceOrder")
+    /*
+    @PostMapping("/PlaceOrderOrchestr")
     public void processOrder(@RequestBody Order order) {
-        callOrderProcessMicroserviceUsingFeignClient(orderId);
-        callPaymentProcessMicroserviceUsingFeignClient(order);
-        callUpdateInventoryMicroserviceUsingFeignClient(order);
+        Boolean status = callOrderProcessMicroserviceUsingFeignClient(orderId);
+        if (status) {
+            Boolean status2 = callPaymentProcessMicroserviceUsingFeignClient(orderId);
+        } else {
+            callFailedPaymentProcessMicroserviceUsingFeignClient(orderId);
+            callFailedOrderProcessMicroserviceUsingFeignClient(orderId);
+        }
+        if (status2) {
+            callUpdateInventoryMicroserviceUsingFeignClient(orderId);
+        } else {
+            callFailedUpdateInventoryMicroserviceUsingFeignClient(orderId);
+            callFailedPaymentProcessMicroserviceUsingFeignClient(orderId);
+            callFailedOrderProcessMicroserviceUsingFeignClient(orderId);
+        }
     }*/
 }
